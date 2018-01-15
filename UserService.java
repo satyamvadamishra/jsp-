800d0ed1b7d5feef43cfc934b88d11652d1e4ba8
satyamvadamishra <35141593+satyamvadamishra@users.noreply.github.com>
2018-01-16 01:33:46 +0530

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,16 @@
+package micky.com.satyam;
+
+public class UserService {
+
+	public void register(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public User validateUser(Login login) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
